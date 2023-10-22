495fa6cbd36ef03fd97e1a50991f09b95fb906a6
AndreaOrca <202110209@gordoncollege.edu.ph>
2023-10-23 01:52:36 +0800

    new login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from "react";
 import './Login.css';
 import LoginForm from '../components/LoginForm';
@@ -5,12 +6,10 @@ import LoginForm from '../components/LoginForm';
 export class Login extends Component {
     render() {
         return (
-            <div className="login_row">
-                <div className="form_container">
-                    <div className="form_content_left"><img src={logo} alt="GC Logo" /></div>
-                    <div className="form_content_right">
-                        <LoginForm/>
-                    </div>
+            <div className="login_page_container">
+                <div class="left_section"></div>
+                <div class="right_section">
+                    <LoginForm/>
                 </div>
             </div>
         );
