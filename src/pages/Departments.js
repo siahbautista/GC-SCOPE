3a7b82ea0a110222cba9da8e222efe322ecd0941
Christian Cuya <cjapaycuya16@gmail.com>
2023-10-23 03:32:13 +0800

    Add for now

--- src/pages/Departments.js
+++ src/pages/Departments.js
@@ -1,10 +1,21 @@
+import Footer from '../components/Footer.js';
 import Header from '../components/Header.js';
 
 function Departments() {
 	return (
     	<div>
     		<Header />
-    		<h1>Departments</h1>
+			{/* Want to change css? Go to global.css*/}
+			<div className="pages_hero_heading_row">
+				<div className="pages_hero_heading_container">
+					<div className="pages_hero_heading">
+						<span className="silhoutte_heading">Departments</span>
+						<h1>Departments</h1>
+					</div>
+				</div>
+			</div>
+			{/* Page Hero Heading End */}
+			<Footer />
     	</div>
     );
 }
