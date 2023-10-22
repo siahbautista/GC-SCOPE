fdf86dec821ced4ddb09171197d1079beff4d121
AndreaOrca <202110209@gordoncollege.edu.ph>
2023-10-23 03:01:33 +0800

    updated login page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-import logo from "../assets/logos/gc-logo.png";
 import "./css-components/LoginForm.css";
+import logo from "../assets/logos/gc-logo.png";
 
 export class LoginForm extends Component {
   render() {
     return (
         <div className="login_form_container">
-            <form className='login_form'>
-                <img src={logo} alt="GC Logo" />
+            <form className="login_form">
+                <img src={logo}></img>
                 <h1>Welcome Students!</h1>
                 <div className="form_inputs">
                     <input id="username" type="text" name="username" className="form_input" placeholder="Username">
@@ -18,12 +18,12 @@ export class LoginForm extends Component {
                     </input>
                 </div>
                 <div className="form_input_fpassword">
-                    <a href="#">Forgot Password</a>
+                    <a href="#">Forgot Password?</a>
                 </div>
                 <button className="login_btn" type="submit">Login</button>
                 <hr className="hr_line"/>
-                <div className="form_input_register">Don't have an account? Click register.</div>
-                <button className="register_btn" type="submit">Register</button>
+                <div className="form_input_register">Don't have an account? Register here.</div>
+                <button className="register_btn" type="submit">Sign Up</button>
             </form>
         </div>
     )
