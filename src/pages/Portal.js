78931cc4d9db36111bae359e190aa2fdac85a629
ndrnnrc <annedreaorca@gmail.com>
2023-10-24 00:57:55 +0800

    login and register page doneee

--- src/pages/Portal.js
+++ src/pages/Portal.js
@@ -1,19 +1,25 @@
 
-import React, { Component } from "react";
-import './css_pages/Login.css';
+import React, {useState} from "react";
+import './css_pages/Portal.css';
 import LoginForm from '../components/LoginForm';
+import RegisterForm from "../components/RegisterForm";
 
-export class Portal extends Component {
-    render() {
-        return (
-            <div className="login_page_container">
-                <div class="left_section"></div>
-                <div class="right_section">
-                    <LoginForm/>
-                </div>
-            </div>
-        );
+function Portal() {
+    const [currentForm, setCurrentForm] = useState('login');
+
+    const toggleForm = (formName) => {
+        setCurrentForm(formName);
     }
+
+    return (
+        <div className="login_page_container">
+            <div class="left_section"></div>
+            <div class="right_section"> {
+                currentForm === "login" ? <LoginForm onFormSwitch={toggleForm}/> : <RegisterForm onFormSwitch={toggleForm}/>
+            }
+            </div>
+        </div>
+    );
 }
 
 export default Portal;
